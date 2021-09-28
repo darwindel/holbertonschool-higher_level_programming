11d1914d8a4aa15ac05adc6942808db73bd16035
Darwin Delgado-Cordova <darwindel00@gmail.com>
2021-09-28 18:10:02 -0400

    Create 3-last_digit.py
    will print last digit

---
+++ 0x00-python-hello_world/3-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+x = number
+if x < 0:
+    x = x * -1
+x = x % 10
+if number < 0:
+    x = x * -1
+if x > 5:
+    print("Last digit of", number, "is", x, "and is greater than 5")
+elif x == 0:
+    print("Last digit of", number, "is", x, "and is 0")
+else:
+    print("Last digit of", number, "is", x, "and is less than 6 and not 0")
