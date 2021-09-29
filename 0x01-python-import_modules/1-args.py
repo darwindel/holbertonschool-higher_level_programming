9fadf1106b0a015e182e97b91aa93cbb580f5524
Darwin Delgado-Cordova <darwindel00@gmail.com>
2021-09-29 17:29:40 -0400

    will print program and empty folder

---
+++ 0x01-python-import_modules/1-args.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+
+
+def main(*argv):
+    i = 0
+    length = len(sys.argv) - 1
+    if length == 1:
+        print("{:d} argument:".format(length))
+    elif length == 0:
+        print("{:d} arguments.".format(length))
+    else:
+        print("{:d} arguments:".format(length))
+    for args in sys.argv:
+        if (i != 0):
+            print("{}:  {}".format(i, args))
+        i += 1
+
+if __name__ == "__main__":
+    main()
