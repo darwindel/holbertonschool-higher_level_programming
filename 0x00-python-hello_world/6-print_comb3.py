ef9762dbdd23edd0681d81bd3d84255320781f85
Darwin Delgado-Cordova <darwindel00@gmail.com>
2021-09-28 23:19:33 -0400

    Create 6-print_comb3.py
    will print

---
+++ 0x00-python-hello_world/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for n in range(0,10):
+    for x in range(0,10):
+        if n == 8 and x == 9:
+                print('89')
+        elif n < x:
+            print("{:d}{:d},".format(n, x), end=" ")
