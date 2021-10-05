1e5c154f4846d3ce3f7694ac726e39d154188900
Darwin Delgado-Cordova <darwindel00@gmail.com>
2021-10-04 21:45:03 -0400

    fixed the variables

--- 0x03-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x03-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     sorted_keys = list(sorted((a_dictionary.keys())))
-    for i in keys:
+    for i in sorted_keys:
         print("{}: {}".format(i, a_dictionary[i]))
