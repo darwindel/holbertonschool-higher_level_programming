13a547ae5e6b79f26a3216a07455442fd31bf84a
Darwin Delgado-Cordova <darwindel00@gmail.com>
2021-10-06 09:46:03 -0400

    added code and permissions

--- 0x04-python-classes/1-square.py
+++ 0x04-python-classes/1-square.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+# 1-square.py
+""" Module for Square with docstring at top"""
+
+
+class Square:
+    """ Makes a size """
+    def __init__(self, size=0):
+    self.__size = size
+
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
