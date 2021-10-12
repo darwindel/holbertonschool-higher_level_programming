3efbd8965f4f4229c2906fabe7ac07dd10051c4f
Darwin Delgado-Cordova <darwindel00@gmail.com>
2021-10-12 15:17:49 -0400

    adding test and code

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+    Adds 2 integers
+    Returns addition of a and b
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Raises TypeError: if either a or b is a non-int/float
+    Returns the addition of a and b
+    """
+    if not isintance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isistance(b, float):
+        raise TypeError("b must be an integer")
+    return a + b
