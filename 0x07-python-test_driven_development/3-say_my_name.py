5fc885d32bf57277623b77f927b8d9e18a2561ad
Darwin Delgado-Cordova <darwindel00@gmail.com>
2021-10-12 22:16:43 -0400

    added doc strings

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -4,7 +4,10 @@
 
 
 def say_my_name(first_name, last_name=""):
-
+    """
+    Returns a string with the full name
+    first_name and last_name must be strings
+    """
     if not isinstance(first_name, str):
         raise TypeError "first_name must be a string"
     if not isinstance(last_name, str):
