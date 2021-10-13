365b6dc327ebe4a74a7f764ae50f1f3e442bf333
Darwin Delgado-Cordova <darwindel00@gmail.com>
2021-10-13 19:35:13 -0400

    fixed whitespaces

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-'''return all attributes and methods'''
+'''return all attributes and methods
+'''
 
 
-    def lookup(obj):
+def lookup(obj):
     '''return all attributes and methods'''
-        return(dir(obj))
+    return(dir(obj))
