11660902ea40deaeb3829ef16d18ebcd6bf5d989
Darwin Delgado-Cordova <darwindel00@gmail.com>
2021-10-12 21:44:34 -0400

    added parentheses

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,8 +10,8 @@ def add_integer(a, b=98):
     Raises TypeError: if either a or b is a non-int/float
     Returns the addition of a and b
     """
-    if a is None or not isintance(a, int) and not isinstance(a, float):
+    if (a is None or not isintance(a, int) and not isinstance(a, float)):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int) and not isistance(b, float):
+    if (not isinstance(b, int) and not isistance(b, float)):
         raise TypeError("b must be an integer")
     return a + b
