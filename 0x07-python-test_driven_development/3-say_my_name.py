790197a4f4e1487264b28a2798568faa683e6194
Darwin Delgado-Cordova <darwindel00@gmail.com>
2021-10-12 22:24:07 -0400

    deleted whitespace

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -9,7 +9,7 @@ def say_my_name(first_name, last_name=""):
     first_name and last_name must be strings
     """
     if not isinstance(first_name, str):
-        raise TypeError ("first_name must be a string")
+        raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
-        raise TypeError ("last_name must be a string")
+        raise TypeError("last_name must be a string")
     print("My name is {first_name} {last_name}")
