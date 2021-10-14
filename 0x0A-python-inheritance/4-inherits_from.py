9acf076a81f5ba8bd6569265674ed12024dec9d1
Darwin Delgado-Cordova <darwindel00@gmail.com>
2021-10-14 13:59:48 -0400

    fixed indents

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-'''
-checks if object is an instance of an inherited class
-'''
+    '''
+    checks if object is an instance of an inherited class
+    '''
 
 
 def inherits_from(obj, a_class):
-'''
-will return true if the object is instance of inherited class
-'''
+    '''
+    will return true if the object is instance of inherited class
+    '''
+
     return(issubclass(type(obj), a_class) and type(obj) is not a_class)
