11c5229db2048bf16c5497f84ae554cce618041e
Darwin Delgado-Cordova <darwindel00@gmail.com>
2021-10-14 13:52:55 -0400

    returns type of class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+checks if an object is an instance of a class
+'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''
+    returns true if obj is an instance of class
+    '''
+    return(isinstance(obj, a_class))
