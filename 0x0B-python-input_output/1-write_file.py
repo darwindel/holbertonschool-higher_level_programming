30ef1c78caac0fbd6b7af67af6cda96c25831d11
Darwin Delgado-Cordova <darwindel00@gmail.com>
2021-10-15 22:52:36 -0400

    will write file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""write a file"""
+
+def write_file(filename="", text""):
+""" read the file and write it """
+    with open(filename, "w") as writefile:
+        writefile.write(text)
+    return (len(text))
