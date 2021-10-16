82714aca7bb8a7fbc19b22a309d5d01a8618cf91
Darwin Delgado-Cordova <darwindel00@gmail.com>
2021-10-15 23:48:00 -0400

    added indent

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""
-append a string at end of text file
-"""
+"""append a string at end of text file"""
 
 
 def append_write(filename="", text=""):
-""" git file to write and append to"""
+    """ git file to write and append to"""
 
     with open(filename, "a") as appendfile:
         appendfile.write(text)
