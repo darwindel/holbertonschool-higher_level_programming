e2c677ba030caf830c2ef90a4b307eda46083283
Darwin Delgado-Cordova <darwindel00@gmail.com>
2021-10-22 00:05:15 -0400

    added code

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -72,4 +72,16 @@ class Rectangle(Base):
             raise ValueError("x must be >= 0")
         self.__x = value
 
-     
+    @property
+    def y(self):
+        """gets y"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """y setter"""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0"
+        self.__y = value 
