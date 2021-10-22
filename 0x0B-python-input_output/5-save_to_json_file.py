d777ae48b9fbb3b21c082e5097ad801168d9b8d6
Darwin Delgado-Cordova <darwindel00@gmail.com>
2021-10-21 23:07:14 -0400

    adding files to repo

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""save json"""
+import json
+
+
+def save_to_json
---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Base.py file"""
+
+
+class Base: 
+    """Base class"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """int id instantiation"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
