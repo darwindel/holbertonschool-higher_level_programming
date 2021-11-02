c5e1e9361597fa204fd3f062f7c0c8b3c0082e2d
Darwin Delgado-Cordova <darwindel00@gmail.com>
2021-11-02 18:06:27 -0400

    added semicolon

--- 0x00-SQL_introduction/9-full_creation.sql
+++ 0x00-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,6 @@
 -- creating a new table
-CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT)
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
+
 INSERT INTO second_table VALUES
 (1, 'John', 10);
 (2, 'Alex', 3);
