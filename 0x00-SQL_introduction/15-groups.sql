d4eaca954f091743ca2ac008c849a980fbf66519
Darwin Delgado-Cordova <darwindel00@gmail.com>
2021-11-02 20:26:05 -0400

    added commas

--- 0x00-SQL_introduction/15-groups.sql
+++ 0x00-SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
---Number of records with same score
-SELECT score, COUNT(score) AS number
-FROM second_table GROUP BY score
+-- Number of records with same score
+SELECT score, COUNT(score) AS number,
+FROM second_table GROUP BY score,
 ORDER BY score DESC;
