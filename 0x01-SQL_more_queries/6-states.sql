5ad20fde03f3401b9d712c4ae87182d3cb9d90e8
Darwin Delgado-Cordova <darwindel00@gmail.com>
2021-11-03 21:00:28 -0400

    Update 6-states.sql

--- 0x01-SQL_more_queries/6-states.sql
+++ 0x01-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- create database and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states.hbtn_0d_usa (
-    id INT AUTO-INCREMENT NOT NULL,
-    name VARCHAR(256) NOT NULL
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+    id INT NOT NULL AUTO-INCREMENT,
+    name VARCHAR(256) NOT NULL,
     PRIMARY KEY(id));
