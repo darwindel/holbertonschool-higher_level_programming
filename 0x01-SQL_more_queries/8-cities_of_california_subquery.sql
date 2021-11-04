42f2c318a96d7e39b2118e144631f775ef24ca9a
Darwin Delgado-Cordova <darwindel00@gmail.com>
2021-11-03 23:56:55 -0400

    Update 8-cities_of_california_subquery.sql

--- 0x01-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x01-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- list cities in cali
-SELECT id, name FROM cities WHERE state_id IS (
+SELECT id, name FROM cities WHERE state_id IN (
     SELECT id FROM states WHERE name LIKE 'California'
     )
 ORDER BY id ASC;
