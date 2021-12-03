65872dbd69fdc5f9b93a42a43e18691a7fc5b2d6
Darwin Delgado-Cordova <darwindel00@gmail.com>
2021-12-03 15:16:26 -0500

    Update 1-hbtn_header.py

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+"""sends url req"""
 import urllib.request
 import sys
 
 if __name__ == "__main__":
-    with urllib.request.urlopen(sys.argv[1]) as response:
-        h = response.getheader('X-Request_Id')
+    with urllib.request.urlopen(sys.argv[1]) as url:
+        h = url.getheader('X-Request_Id')
         print("{}".format(h))
