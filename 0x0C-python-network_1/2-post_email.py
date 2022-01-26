08e0707f7132ab756bf1e107f1a923fdf14c25ca
Darwin Delgado-Cordova <darwindel00@gmail.com>
2022-01-26 12:52:04 -0500

    added l to urllib

--- 0x0C-python-network_1/2-post_email.py
+++ 0x0C-python-network_1/2-post_email.py
@@ -5,7 +5,7 @@ import sys
 
 if __name__ == "__main__":
     email = sys.argv[2]
-    data = urlib.parse.urlencode(({"email": email}).encode('ascii'))
+    data = urllib.parse.urlencode(({"email": email}).encode('ascii'))
     with urllib.request.urlopen(sys.argv[1], data) as url:
         s = url.read()
         print(s.decode('utf-8'))
