ca3a9edcc5b96adb275c722322df076903a16865
Darwin Delgado-Cordova <darwindel00@gmail.com>
2022-01-26 13:23:16 -0500

    Create 3-error_code.py

---
+++ 0x0C-python-network_1/3-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""3-error_code.py"""
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(argv[1]) as url:
+            s.url.read()
+            print(s.decode("utf-8"))
+     except urllib.error.HTTPError as message:
+        print('Error code: {}".formtat(message))
