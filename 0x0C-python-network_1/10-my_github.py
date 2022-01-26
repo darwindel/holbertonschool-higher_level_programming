5db0d29284f38e21ea427c91c4005179ef6942ab
Darwin Delgado-Cordova <darwindel00@gmail.com>
2022-01-26 17:32:28 -0500

    Update 10-my_github.py

--- 0x0C-python-network_1/10-my_github.py
+++ 0x0C-python-network_1/10-my_github.py
@@ -6,7 +6,7 @@ import sys
 
 if __name__ == "__main__":
     user = sys.argv[1]
-    pwd = sys.argv[2]
-    req = requests.get('https://api.github.com/user',
-                        auth=(user, password))
+    pw = sys.argv[2]
+    r = requests.get('https://api.github.com/user',
+                     auth=(user, pw))
     print(r.json().get('id'))
