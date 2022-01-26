5a60acdc181811cb491b2195fb33a51c680fc7f1
Darwin Delgado-Cordova <darwindel00@gmail.com>
2022-01-26 12:57:28 -0500

    Update 2-post_email.py

--- 0x0C-python-network_1/2-post_email.py
+++ 0x0C-python-network_1/2-post_email.py
@@ -6,6 +6,6 @@ import sys
 if __name__ == "__main__":
     email = sys.argv[2]
     data = urllib.parse.urlencode({'email': email}).encode('ascii')
-    with urllib.request.urlopen(sys.argv[1], data) as url:
-        s = url.read()
+    with urllib.request.urlopen(sys.argv[1], data) as email:
+        s = email.read()
         print(s.decode('utf-8'))
